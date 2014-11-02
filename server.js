3cee4502998285671546d78a0f2db9bb745a29a2
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-11-02 08:16:38 -0500

    Click ability to send to school data page.

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ var Router = require('routes-router')
 
 function ready () {
   var router = Router()
-    , schools = Schools(dat)
+    , schools = new Schools(dat)
 
   router.addRoute('/data/philly.geo.json', function (req, res) {
     fs.createReadStream(__dirname + '/data/philly.geo.json').pipe(res)
   })
 
-  router.addRoute('/data/schools.geo.json', schools)
+  router.addRoute('/data/schools.geo.json', function (req, res) {
+    schools.all().pipe(res)
+  })
+
+  router.addRoute('/style.css', function (req, res) {
+    fs.createReadStream(__dirname + '/style.css').pipe(res)
+  })
+
+  router.addRoute('/school/:id', function (req, res, opts) {
+    schools.get(opts.params.id).pipe(res)
+  })
 
   function index (req, res) {
     fs.createReadStream(__dirname + '/index.html').pipe(res)
